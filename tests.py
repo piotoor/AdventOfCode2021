24543e204fb827eb4358915b49c900d2db51b762
Piotr Kozka <piotoor@gmail.com>
2022-01-04 22:10:23 +0100

    Added testcases to day1 tests

--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ class Day1(unittest.TestCase):
     @parameterized.expand([
         ("example 1", [], 0),
         ("example 2", [199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7),
+        ("example 3 puzzle data", day1.parse_day1_data(), 1121),
     ])
     def test_count_number_of_increases(self, _, data, expected):
         self.assertEqual(expected, day1.count_number_of_increases(data))
@@ -18,6 +19,7 @@ class Day1(unittest.TestCase):
         ("example 3", [1, 10], 0),
         ("example 4", [1, 10, 100], 0),
         ("example 5", [199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 5),
+        ("example 6 puzzle data", day1.parse_day1_data(), 1065),
     ])
     def test_count_number_of_increased_windows(self, _, data, expected):
         self.assertEqual(expected, day1.count_number_of_increased_windows(data))
