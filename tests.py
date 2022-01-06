bd6ac08c5b14c4ca137d5a41b31183226206cf1f
Piotr Kozka <piotoor@gmail.com>
2022-01-06 21:54:26 +0100

    DRY changes to day4 tests

--- tests.py
+++ tests.py
@@ -89,8 +89,7 @@ class Day3(unittest.TestCase):
 
 
 class Day4(unittest.TestCase):
-    @parameterized.expand([
-        ("example 1", [
+    data = [
             [[22, 13, 17, 11, 0],
              [8, 2, 23, 4, 24],
              [21, 9, 14, 16, 7],
@@ -108,7 +107,11 @@ class Day4(unittest.TestCase):
              [18, 8, 23, 26, 20],
              [22, 11, 13, 6, 5],
              [2, 0, 12, 3, 7]]
-        ], [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1], 4512),
+    ]
+
+    @parameterized.expand([
+        ("example 1", data,
+         [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1], 4512),
         ("example 2 puzzle data", day4.parse_day4_data()[0], day4.parse_day4_data()[1], 89001),
     ])
     def test_get_first_winner_points(self, _, boards, nums, expected):
@@ -119,25 +122,8 @@ class Day4(unittest.TestCase):
         self.assertEqual(expected, bingo.get_winner_points())
 
     @parameterized.expand([
-        ("example 1", [
-            [[22, 13, 17, 11, 0],
-             [8, 2, 23, 4, 24],
-             [21, 9, 14, 16, 7],
-             [6, 10, 3, 18, 5],
-             [1, 12, 20, 15, 19]],
-
-            [[3, 15, 0, 2, 22],
-             [9, 18, 13, 17, 5],
-             [19, 8, 7, 25, 23],
-             [20, 11, 10, 24, 4],
-             [14, 21, 16, 12, 6]],
-
-            [[14, 21, 17, 24, 4],
-             [10, 16, 15, 9, 19],
-             [18, 8, 23, 26, 20],
-             [22, 11, 13, 6, 5],
-             [2, 0, 12, 3, 7]]
-        ], [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1], 1924),
+        ("example 1", data,
+         [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1], 1924),
         ("example 2 puzzle data", day4.parse_day4_data()[0], day4.parse_day4_data()[1], 7296),
     ])
     def test_get_last_winner_points(self, _, boards, nums, expected):
