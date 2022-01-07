a63adf7bd26e41956de63859e551b046d585c61a
Piotr Kozka <piotoor@gmail.com>
2022-01-07 14:21:54 +0100

    Added testcases to day18 tests

--- tests.py
+++ tests.py
@@ -826,10 +826,17 @@ class Day18(unittest.TestCase):
         ("aoc example 2", [[1, 0], [9, 0]], 21),
         ("aoc example 3", [[9, 1], [1, 1], [1, 1], [9, 1]], 129),
         ("aoc example 4", [[1, 1], [2, 1], [3, 2], [4, 2], [5, 1]], 143),
-        ("aoc example 5", [[0, 3], [7, 3], [4, 2], [7, 3], [8, 3], [6, 3], [0, 3], [8, 1], [1, 1]], 1384)
+        ("aoc example 5", [[0, 3], [7, 3], [4, 2], [7, 3], [8, 3], [6, 3], [0, 3], [8, 1], [1, 1]], 1384),
     ])
     def test_calculate_magnitude(self, _, data, expected):
         self.assertEqual(expected, day18.SnailfishCalculator.calculate_magnitude(data))
+
+    @parameterized.expand([
+        ("puzzle data", day18.parse_day18_data(), 2501)
+    ])
+    def test_calculate_total_magnitude(self, _, data, expected):
+        calc = day18.SnailfishCalculator(data)
+        self.assertEqual(expected, calc.calculate_total_magnitude())
 
     @parameterized.expand([
         ("aoc example 1", [
@@ -844,6 +851,7 @@ class Day18(unittest.TestCase):
             [[2, 1], [7, 3], [7, 3], [7, 2], [5, 2], [8, 2], [9, 3], [3, 3], [0, 3], [2, 3]],
             [[5, 3], [2, 3], [5, 2], [8, 2], [3, 3], [7, 3], [5, 2], [7, 3], [5, 3], [4, 2], [4, 2]]
         ], 3993),
+        ("puzzle data", day18.parse_day18_data(), 4935)
     ])
     def test_calculate_largest_sum_of_two_numbers(self, _, data, expected):
         calc = day18.SnailfishCalculator(data)
